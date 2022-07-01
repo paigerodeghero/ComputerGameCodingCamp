57406596584ad5989a09131b2a4efa0fb899b3e0
Conner Phillis <conner.phillis@keymarkinc.com>
2022-06-30 21:03:45 -0400

    fix up code for skills map

--- website/src/skill-map/elements.ts
+++ website/src/skill-map/elements.ts
@@ -71,7 +71,7 @@ const nodes = {
     },
     position,
   },
-  CreatingObstacles: {
+  creatingObstacles: {
     id: "creating-obstacles",
     type: "skillTreeNode",
     data: {
@@ -80,7 +80,7 @@ const nodes = {
     },
     position,
   },
-  ObstacleCollision: {
+  obstacleCollision: {
     id: "obstacle-collision",
     type: "skillTreeNode",
     data: {
@@ -90,7 +90,7 @@ const nodes = {
     },
     position,
   },
-  KeepingScore: {
+  keepingScore: {
     id: "keeping-score",
     type: "skillTreeNode",
     data: {
@@ -99,7 +99,7 @@ const nodes = {
     },
     position,
   },
-  KeepingLives: {
+  keepingLives: {
     id: "keeping-lives",
     type: "skillTreeNode",
     data: {
@@ -108,7 +108,7 @@ const nodes = {
     },
     position,
   },
-  Explosions: {
+  explosions: {
     id: "Explosions",
     type: "skillTreeNode",
     data: {
@@ -117,7 +117,7 @@ const nodes = {
     },
     position,
   },
-  Shooting: {
+  shooting: {
     id: "Shooting",
     type: "skillTreeNode",
     data: {
@@ -135,7 +135,7 @@ const nodes = {
     },
     position,
   },
-  SideScrolling: {
+  sideScrolling: {
     id: "SideScrolling",
     type: "skillTreeNode",
     data: {
@@ -143,7 +143,7 @@ const nodes = {
     },
     position
   },
-  Signals: {
+  signals: {
     id: "Signals",
     type: "skillTreeNode",
     data: {
@@ -244,152 +244,38 @@ const makeConnection = (a: Node, b: Node): Edge => ({
 })
 
 export const initialEdges: Edge[] = [
-  {
-    id: makeEdgeId(nodes.whatIsGodot, nodes.navigatingMacFileSystem),
-    source: nodes.whatIsGodot.id,
-    target: nodes.navigatingMacFileSystem.id,
-  },
-  {
-    id: makeEdgeId(nodes.whatIsGodot, nodes.navigatingWindowsFileSystem),
-    source: nodes.whatIsGodot.id,
-    target: nodes.navigatingWindowsFileSystem.id,
-  },
-  {
-    id: makeEdgeId(
-      nodes.navigatingMacFileSystem,
-      nodes.settingUpProjectEnvironment
-    ),
-    source: nodes.navigatingMacFileSystem.id,
-    target: nodes.settingUpProjectEnvironment.id,
-  },
-  {
-    id: makeEdgeId(
-      nodes.navigatingWindowsFileSystem,
-      nodes.settingUpProjectEnvironment
-    ),
-    source: nodes.navigatingWindowsFileSystem.id,
-    target: nodes.settingUpProjectEnvironment.id,
-  },
-  {
-    id: makeEdgeId(nodes.settingUpProjectEnvironment, nodes.importingCharacter),
-    source: nodes.settingUpProjectEnvironment.id,
-    target: nodes.importingCharacter.id,
-  },
-  {
-    id: makeEdgeId(nodes.importingCharacter, nodes.usingVsCode),
-    source: nodes.importingCharacter.id,
-    target: nodes.usingVsCode.id,
-  },
-  {
-    id: makeEdgeId(nodes.usingVsCode, nodes.characterMovement),
-    source: nodes.usingVsCode.id,
-    target: nodes.characterMovement.id,
-  },
-  {
-    id: makeEdgeId(nodes.characterMovement, nodes.CreatingObstacles),
-    source: nodes.characterMovement.id,
-    target: nodes.CreatingObstacles.id,
-  },
-  {
-    id: makeEdgeId(nodes.CreatingObstacles, nodes.ObstacleCollision),
-    source: nodes.CreatingObstacles.id,
-    target: nodes.ObstacleCollision.id,
-  },
-  {
-    id: makeEdgeId(nodes.ObstacleCollision, nodes.KeepingScore),
-    source: nodes.ObstacleCollision.id,
-    target: nodes.KeepingScore.id,
-  },
-  {
-    id: makeEdgeId(nodes.KeepingScore, nodes.KeepingLives),
-    source: nodes.KeepingScore.id,
-    target: nodes.KeepingLives.id,
-  },
-  {
-    id: makeEdgeId(nodes.KeepingLives, nodes.Explosions),
-    source: nodes.KeepingLives.id,
-    target: nodes.Explosions.id,
-  },
-  {
-    id: makeEdgeId(nodes.Explosions, nodes.Shooting),
-    source: nodes.Explosions.id,
-    target: nodes.Shooting.id,
-  },
-  {
-    id: makeEdgeId(nodes.Shooting, nodes.dying),
-    source: nodes.Shooting.id,
-    target: nodes.dying.id,
-  },
-  {
-    id: makeEdgeId(nodes.characterMovement, nodes.sceneMovement),
-    source: nodes.characterMovement.id,
-    target: nodes.sceneMovement.id,
-  },
-  {
-    id: makeEdgeId(nodes.dying, nodes.SideScrolling),
-    source: nodes.dying.id,
-    target: nodes.SideScrolling.id
-  },
-  {
-    id: makeEdgeId(nodes.SideScrolling, nodes.buildingAndSharingGames),
-    source: nodes.SideScrolling.id,
-    target: nodes.buildingAndSharingGames.id
-  },
-  {
-    id: makeEdgeId(nodes.sceneMovement, nodes.SideScrolling),
-    source: nodes.sceneMovement.id,
-    target: nodes.SideScrolling.id,
-  },
-  {
-    id: makeEdgeId(nodes.dying, nodes.Signals),
-    source: nodes.dying.id,
-    target: nodes.Signals.id
-  },
-  {
-    id: makeEdgeId(nodes.sceneMovement, nodes.Signals),
-    source: nodes.sceneMovement.id,
-    target: nodes.Signals.id
-  },
-  {
-    id: makeEdgeId(nodes.Signals, nodes.uiAndPausing),
-    source: nodes.Signals.id,
-    target: nodes.uiAndPausing.id
-  },
-  {
-    id: makeEdgeId(nodes.uiAndPausing, nodes.buildingAndSharingGames),
-    source: nodes.uiAndPausing.id,
-    target: nodes.buildingAndSharingGames.id
-  },
-  {
-    id: makeEdgeId(nodes.dying, nodes.obstacleSpawning),
-    source: nodes.dying.id,
-    target: nodes.obstacleSpawning.id
-  },
-  {
-    id: makeEdgeId(nodes.sceneMovement, nodes.obstacleSpawning),
-    source: nodes.sceneMovement.id,
-    target: nodes.obstacleSpawning.id
-  },
-  {
-    id: makeEdgeId(nodes.obstacleSpawning, nodes.buildingAndSharingGames),
-    source: nodes.obstacleSpawning.id,
-    target: nodes.buildingAndSharingGames.id
-  },
-  {
-    id: makeEdgeId(nodes.dying, nodes.additionalFeatures),
-    source: nodes.dying.id,
-    target: nodes.additionalFeatures.id,
-  },
-  {
-    id: makeEdgeId(nodes.sceneMovement, nodes.additionalFeatures),
-    source: nodes.sceneMovement.id,
-    target: nodes.addingSounds.id,
-  },
-  {
-    id: makeEdgeId(nodes.additionalFeatures, nodes.buildingAndSharingGames),
-    source: nodes.additionalFeatures.id,
-    target: nodes.buildingAndSharingGames.id
-  },
+  makeConnection(nodes.whatIsGodot, nodes.navigatingMacFileSystem),
+  makeConnection(nodes.whatIsGodot, nodes.navigatingWindowsFileSystem),
+  makeConnection(nodes.navigatingMacFileSystem, nodes.settingUpProjectEnvironment),
+  makeConnection(nodes.navigatingWindowsFileSystem, nodes.settingUpProjectEnvironment),
+  makeConnection(nodes.settingUpProjectEnvironment, nodes.importingCharacter),
+  makeConnection(nodes.importingCharacter, nodes.usingVsCode),
+  makeConnection(nodes.usingVsCode, nodes.characterMovement),
+  makeConnection(nodes.characterMovement, nodes.creatingObstacles),
+  makeConnection(nodes.creatingObstacles, nodes.obstacleCollision),
+  makeConnection(nodes.obstacleCollision, nodes.keepingScore),
+  makeConnection(nodes.keepingScore, nodes.keepingLives),
+  makeConnection(nodes.keepingLives, nodes.explosions),
+  makeConnection(nodes.explosions, nodes.shooting),
+  makeConnection(nodes.shooting, nodes.dying),
+  makeConnection(nodes.characterMovement, nodes.sceneMovement),
+
+  makeConnection(nodes.dying, nodes.sideScrolling),
+  makeConnection(nodes.sideScrolling, nodes.buildingAndSharingGames),
+  makeConnection(nodes.dying, nodes.signals),
+
+  makeConnection(nodes.sceneMovement, nodes.signals),
+  makeConnection(nodes.signals, nodes.uiAndPausing),
+  makeConnection(nodes.uiAndPausing, nodes.buildingAndSharingGames),
+
+  makeConnection(nodes.dying, nodes.obstacleSpawning),
+  makeConnection(nodes.sceneMovement, nodes.obstacleSpawning),
+  makeConnection(nodes.obstacleSpawning, nodes.buildingAndSharingGames),
+
+  makeConnection(nodes.dying, nodes.additionalFeatures),
+  makeConnection(nodes.sceneMovement, nodes.additionalFeatures),
+  makeConnection(nodes.additionalFeatures, nodes.buildingAndSharingGames),
+
   makeConnection(nodes.dying, nodes.gameStoryLine),
   makeConnection(nodes.sceneMovement, nodes.gameStoryLine),
   makeConnection(nodes.gameStoryLine, nodes.buildingAndSharingGames),
